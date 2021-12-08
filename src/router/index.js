a43061b88d64cf7d945b44fd7cc0a919141895b5
Davi Rodrigues <davirds07@gmail.com>
2021-12-08 20:29:32 -0300

    Setup vue

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/Home"
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "Home",
+            component: Home,
+        },
+    ],
+});
+
+export default router;
