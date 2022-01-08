1c3b924e54c96b6aeaaa92005c363dcb79d34a6f
sambarboza <sambarbosaa@gmail.com>
2022-01-08 17:50:42 -0500

    lbp route

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home"
+import LBP from "@/pages/LBP"
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "Home",
-            component: Home,
-        },
-    ],
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: "/lbp",
+      name: "LBP",
+      component: LBP,
+    },
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+  ],
 });
 
 export default router;
