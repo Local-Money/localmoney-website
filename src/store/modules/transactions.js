925793731bc1641eb7a84933b21dd3ff85f6f2dc
Davi Rodrigues <davirds07@gmail.com>
2022-01-11 00:26:19 -0300

    add methods to fetch lbp informations

--- src/store/modules/transactions.js
+++ src/store/modules/transactions.js
@@ -1,19 +1,15 @@
-import {
-  Extension,
-  LCDClient,
-  MsgExecuteContract,
-  StdSignature,
-  StdSignMsg,
-  StdTx,
-} from "@terra-money/terra.js";
-import {FACTORY_CONTRACT} from "@/constants";
+import { FACTORY_CONTRACT } from "../../constants";
+import { connectExtension } from "@/terra/extension";
+import { buildClient } from "../../terra/client";
+import { getBalance, getLBPs, getReverseSimulation, getSimulation, getTokenBalance, getTokenInfo, getWeights } from "../../terra/queries";
+import { PAIR_CONTRACT } from "../../constants";
+import { nativeTokenFromPair, saleAssetFromPair } from "../../helpers/asset_pairs";
+import { Dec } from "@terra-money/terra.js";
 
-const lcdOptions = {
+let terra = buildClient({
   URL: 'http://143.244.190.1:3060',
-  chainID: 'localterra',
-}
-let terra = new LCDClient(lcdOptions);
-const ext = new Extension()
+  chainID: "localterra"
+});
 
 const state = {
   loading: {
@@ -22,112 +18,107 @@ const state = {
     transaction: "Follow the transaction"
   },
   walletAddress: '',
-  factoryConfig: {},
+  balance: 0,
+  tokenBalance: 0,
+  tokenPrice: 0,
+  currentPair: {},
+  pairWeights: {},
+  saleTokenInfo: {},
 };
-
-// eslint-disable-next-line no-unused-vars
-function prepareTransaction(signedMsg) {
-  const {public_key, signature, stdSignMsgData} = signedMsg;
-  const sig = StdSignature.fromData({
-    signature,
-    pub_key: {
-      type: "tendermint/PubKeySecp256k1",
-      value: public_key,
-    },
-  });
-
-  const stdSignMsg = StdSignMsg.fromData(stdSignMsgData);
-  return new StdTx(stdSignMsg.msgs, stdSignMsg.fee, [sig], stdSignMsg.memo);
-}
 
 const getters = {
   walletAddress: (state) => state.walletAddress,
+  balance: (state) => state.balance,
+  tokenBalance: (state) => state.tokenBalance,
+  tokenPrice: (state) => state.tokenPrice,
+  pairWeights: (state) => state.pairWeights,
+  currentPair: (state) => state.currentPair,
+  saleTokenInfo: (state) => state.saleTokenInfo,
   loading: (state) => state.loading,
 };
 
-const actions = {
-  async initWallet({commit, dispatch}) {
-    const ext = new Extension()
-    const res = await ext.request('connect')
-    const info = await ext.request('info')
-    terra = new LCDClient({
-      URL: info.payload.lcd,
-      chainID: info.payload.chainID
-    })
-    const walletAddress = res.payload.address
-    commit('setWalletAddress', walletAddress)
-    dispatch('fetchFactoryConfig')
-  },
-  /**
-   * Fetch Factory Contract config
-   */
-  async fetchFactoryConfig({commit, dispatch}) {
-    const cfgQuery = {config: {}};
-    const factoryConfig = await terra.wasm.contractQuery(
-      FACTORY_CONTRACT,
-      cfgQuery
-    );
-    commit("setFactoryConfig", factoryConfig);
-    dispatch("fetchOffers");
-    dispatch("fetchTradeInfos")
-  },
-  async swap({commit, getters, dispatch}) {
-    const swapMsg = {
-      offer_asset: {
-        info: {
-          native_token: {
-            denom: "uusd"
-          }
-        },
-        amount: 5 * 1_000_000,
-      },
-      max_spread: 0.01
-    }
-    const msg = new MsgExecuteContract(getters.walletAddress, getters.poolAddr, swapMsg)
-    await executeMsg(commit, getters, dispatch, msg)
-    console.log('swapped')
-  }
-};
-
-async function executeMsg(commit, getters, dispatch, msg) {
-  if (getters.walletAddress === "") {
-    dispatch('initWallet')
-    return
-  }
-  return new Promise((resolve) => {
-    ext.once('onPost', async (res) => {
-      if (res.success) {
-        commit("setLoadingTransaction", res.result.txhash)
-        commit("setIsLoading", true)
-      }
-      let interval = setInterval(async () => {
-        let txInfo = await terra.tx.txInfo(res.result.txhash)
-        if (txInfo) {
-          resolve(txInfo)
-          clearInterval(interval)
-          commit("setIsLoading", false)
-        }
-      }, 1000)
-    })
-    ext.post({
-      msgs: [msg]
-    })
-    /*
-    //Suddenly stopped working (at least on Terrarium, needs to be tested on TestNet, MainNet.
-    //Or hopefully we'll be able to use the "auto" fee option.
-    terra.tx.estimateFee(getters.walletAddress, [msg]).then((stdFee) => {
-      ext.post({
-        fee: stdFee,
-        msgs: [msg]
-      })
-    })
-     */
-  })
-}
-
 const mutations = {
   setWalletAddress: (state, walletAddress) => (state.walletAddress = walletAddress),
-  setFactoryConfig: (state, factoryConfig) => (state.factoryConfig = factoryConfig)
+  setBalance: (state, balance) => (state.balance = balance),
+  setTokenBalance: (state, tokenBalance) => (state.tokenBalance = tokenBalance),
+  setFactoryConfig: (state, factoryConfig) => (state.factoryConfig = factoryConfig),
+  setTokenPrice: (state, tokenPrice) => (state.tokenPrice = tokenPrice),
+  setPairWeights: (state, pairWeights) => (state.pairWeights = pairWeights),
+  setCurrentPair: (state, currentPair) => (state.currentPair = currentPair),
+  setSaleTokenInfo: (state, saleTokenInfo) => (state.saleTokenInfo = saleTokenInfo),
+};
+
+const actions = {
+  async initWallet({ commit, dispatch }) {
+    const { wallet, info } = await connectExtension()
+    terra = buildClient({
+      URL: info.lcd,
+      chainID: info.chainID
+    })
+    commit("setWalletAddress", wallet.address)
+    dispatch("fetchCurrentPair")
+  },
+  async fetchCurrentPair({ commit, dispatch }) {
+    const lbps = (await getLBPs(terra, FACTORY_CONTRACT)).filter(
+      lbp => lbp.contract_addr == PAIR_CONTRACT
+    )
+    //const currentTime = Math.floor(Date.now() / 1000);
+    const currentPair = lbps.find(
+      (lbp) => lbp.contract_addr == PAIR_CONTRACT
+      //(lbp) => lbp.start_time <= currentTime && lbp.end_time > currentTime
+    )
+    commit("setCurrentPair", currentPair)
+    dispatch("fetchSaleTokenInfo")
+    dispatch("fetchWeights")
+    dispatch("fetchTokenBalance")
+    dispatch("fetchBalance")
+    //dispatch("fetchTokenPrice")
+  },
+  async fetchSaleTokenInfo({ getters, commit }) {
+    const saleTokenAddress = saleAssetFromPair(getters.currentPair.asset_infos).info.token.contract_addr
+    const saleTokenInfo = await getTokenInfo(terra, saleTokenAddress)
+    commit("setSaleTokenInfo", saleTokenInfo)
+  },
+  async fetchWeights({ getters, commit }) {
+    const pair = getters.currentPair
+    const nativeToken = nativeTokenFromPair(pair.asset_infos).info.native_token.denom
+    const [nativeTokenWeight, saleTokenWeight] = await getWeights(terra, pair.contract_addr, nativeToken)
+    commit("setPairWeights", { nativeTokenWeight, saleTokenWeight })
+  },
+  async fetchBalance({ getters, commit }) {
+    const walletAddress = getters.walletAddress
+    if (walletAddress.length === 0) {
+      const pair = getters.currentPair
+      const nativeToken = nativeTokenFromPair(pair.asset_infos).info.native_token.denom;
+      const balance = await getBalance(terra, nativeToken, walletAddress)
+      commit("setBalance", balance)
+    }
+  },
+  async fetchTokenBalance({ getters, commit }) {
+    const walletAddress = getters.walletAddress
+    if (walletAddress.length === 0) {
+      const pair = getters.currentPair
+      const tokenAddress = saleAssetFromPair(pair.asset_infos).info.token.contract_addr
+      const tokenBalance = await getTokenBalance(terra, tokenAddress, walletAddress)
+      commit("setTokenBalance", tokenBalance)
+    }
+  },
+  async fetchTokenPrice({ dispatch, commit }) {
+    const oneUst = (new Dec(1)).mul(10 ** 6).toInt()
+    console.log(oneUst)
+    const tokenPrice = await dispatch("getReverseSimulation", oneUst)
+    commit("setTokenPrice", tokenPrice)
+  },
+  async getSimulation({ getters }, amount) {
+    const pair = getters.currentPair
+    const assetInfo = nativeTokenFromPair(pair.asset_infos).info
+    return await getSimulation(terra, pair.contract_addr, amount, assetInfo)
+  },
+  async getReverseSimulation({ getters }, amount) {
+    const pair = getters.currentPair
+    const assetInfo = saleAssetFromPair(pair.asset_infos).info
+    return await getReverseSimulation(terra, pair.contract_addr, amount, assetInfo)
+  },
 };
 
 export default {
