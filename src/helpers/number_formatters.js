de9294b648de58fc668891c23c37c6c9420c7fa7
Davi Rodrigues <davirds07@gmail.com>
2022-01-11 22:04:39 -0300

    Clean number_formatters helper

--- src/helpers/number_formatters.js
+++ src/helpers/number_formatters.js
@@ -4,24 +4,24 @@ export function formatUSD(amount) {
   const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
 
   return formatter.format(amount)
-};
+}
 
-export function formatNumber(amount, opts={}) {
-  return new Intl.NumberFormat(undefined, opts).format(amount);
-};
+export function formatNumber(amount, opts = {}) {
+  return new Intl.NumberFormat(undefined, opts).format(amount)
+}
 
 export function formatTokenAmount(amount, decimals) {
-  const tokens = Dec.withPrec(amount, decimals);
-  const formatter = new Intl.NumberFormat(undefined, { maximumSignificantDigits: String(amount).length });
+  const tokens = Dec.withPrec(amount, decimals)
+  const formatter = new Intl.NumberFormat(undefined, { maximumSignificantDigits: String(amount).length })
   const parts = formatter.formatToParts(parseFloat(tokens.toString()));
 
-  if(parts[parts.length-1].type === 'fraction') {
-    parts[parts.length-1].value = tokens.toFixed(decimals).split('.')[1];
+  if (parts[parts.length - 1].type === 'fraction') {
+    parts[parts.length - 1].value = tokens.toFixed(decimals).split('.')[1]
   }
 
-  return parts.map(part => part.value).join('');
+  return parts.map(part => part.value).join('')
 }
 
 export function dropInsignificantZeroes(numString) {
-  return numString.replace(/(\.\d*?)0*$/, '$1').replace(/\.$/, '');
+  return numString.replace(/(\.\d*?)0*$/, '$1').replace(/\.$/, '')
 }
