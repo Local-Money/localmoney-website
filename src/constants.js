c0e7ed446f5162eafc7835116921cbb449aaffe0
Davi Rodrigues <davirds07@gmail.com>
2022-01-13 19:57:14 -0300

    Update pair contract

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const FACTORY_CONTRACT = "terra12s74xfyk7uz9pkruqt5d9cm8s3rzjuqpv5rg8r";
-export const PAIR_CONTRACT = "terra1s9z2mnszsagj5g62cn0e2gak2a3xkekx28hn50";
+export const PAIR_CONTRACT = "terra15te98vtgw0r8jjyau8vx3htd67z0724x6eh3nt";
