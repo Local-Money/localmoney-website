62f185cbe004b2c9ecea265620a79a474edf7159
Davi Rodrigues <davirds07@gmail.com>
2022-01-16 19:36:44 -0300

    Add new pair contract addr

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const FACTORY_CONTRACT = "terra12s74xfyk7uz9pkruqt5d9cm8s3rzjuqpv5rg8r";
-export const PAIR_CONTRACT = "terra15te98vtgw0r8jjyau8vx3htd67z0724x6eh3nt";
+export const PAIR_CONTRACT = "terra1nlwn4y0c4svqq7dp0z3z23lzjx2n5fe03j6x3p";
