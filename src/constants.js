97a27ee04e78b7750bf0aa0bdec629d298d1eb7b
sambarboza <sambarbosaa@gmail.com>
2022-01-22 07:29:57 -0500

    new contracts

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const FACTORY_CONTRACT = "terra16cgu3q8hn275mr0n43mdtyc5mj3td0w22w2qtf";
-export const PAIR_CONTRACT = "terra12nlf0qm945gr45r3rn2322ka8d44c2qjzn5mhf";
+export const FACTORY_CONTRACT = "terra1wzt06h3zrfy7rjhyxptfnc5f0r8r2jre2kp9fy";
+export const PAIR_CONTRACT = "terra17wn9yfvl3sx9003mjvs4wu02sg5wta5sx7hjvw";
