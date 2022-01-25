5b12fcabbd44804912612c5715a88537b896477a
Davi Rodrigues <davirds07@gmail.com>
2022-01-24 22:54:07 -0300

    New contracts

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const FACTORY_CONTRACT = "terra1wzt06h3zrfy7rjhyxptfnc5f0r8r2jre2kp9fy";
-export const PAIR_CONTRACT = "terra17wn9yfvl3sx9003mjvs4wu02sg5wta5sx7hjvw";
+export const FACTORY_CONTRACT = "terra16cgu3q8hn275mr0n43mdtyc5mj3td0w22w2qtf";
+export const PAIR_CONTRACT = "terra1tgd9trva9lrn7due4xn8ytv7y8uk4rd89lldnp";
