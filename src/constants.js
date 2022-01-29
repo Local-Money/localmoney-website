1c5aaa43dfd451e8245c7965734ff689630e8ba1
sambarboza <sambarbosaa@gmail.com>
2022-01-28 21:12:09 -0500

    new lbp

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const FACTORY_CONTRACT = "terra16cgu3q8hn275mr0n43mdtyc5mj3td0w22w2qtf";
-export const PAIR_CONTRACT = "terra1tgd9trva9lrn7due4xn8ytv7y8uk4rd89lldnp";
+export const FACTORY_CONTRACT = "terra1wzt06h3zrfy7rjhyxptfnc5f0r8r2jre2kp9fy";
+export const PAIR_CONTRACT = "terra1h5wr44ncn0hmlwxcwzftl238w0uacvk853ejdr";
