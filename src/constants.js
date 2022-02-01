dc7bcefe17c63343bd130753f66406f3cdefdea8
Samuel Barbosa <sambarbosaa@gmail.com>
2022-02-01 12:30:26 -0500

    new contract address

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const FACTORY_CONTRACT = "terra1wzt06h3zrfy7rjhyxptfnc5f0r8r2jre2kp9fy";
-export const PAIR_CONTRACT = "terra1h5wr44ncn0hmlwxcwzftl238w0uacvk853ejdr";
+export const PAIR_CONTRACT = "terra1t279cl3hv8zlqu9969dkv7x2fac3ftm3yyls62";
