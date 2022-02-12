41b6f0e8ab2cd94692844806b08f5848ca15071a
Gabriel Brettas <gabrielbrettas@gmail.com>
2022-02-12 14:42:30 -0300

    Fixed responsive layout

--- src/components/ModalFeedback.vue
+++ src/components/ModalFeedback.vue
@@ -1,6 +1,21 @@
 <template>
   <Modal @close="$emit('close')" :modalActive="modalFeedback.show">
     <div class="modal-content card">
+      <div class="close-btn" @click="$emit('close')">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke="inherit"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+      </div>
       <div class="content">
         <div class="wrap-img">
           <img
@@ -10,21 +25,6 @@
           />
         </div>
         <div class="content-info">
-          <div class="close-btn" @click="$emit('close')">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke="inherit"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-          </div>
           <h2 v-html="modalFeedback.title" />
           <p v-html="modalFeedback.message" />
           <div class="extra">
@@ -56,7 +56,7 @@ export default {
 
 export function successFeedback(
   title = "Awesome!",
-  message = "Thanks for participating. You are now part of the <span class='text-primary'>LOCAL</span> community. "
+  message = "Thanks for participating.</br>You are now part of the <span class='text-primary'>LOCAL</span> community. "
 ) {
   return baseFeedback(true, true, title, message);
 }
@@ -95,14 +95,20 @@ export function baseFeedback(
   background-color: $gray150;
   margin-top: 10%;
   z-index: 100;
+  .close-btn {
+    text-align: right;
+    cursor: pointer;
+    stroke: $primary;
+  }
   .content {
     display: flex;
     .wrap-img {
       width: 40%;
       display: flex;
       align-items: center;
+      padding: 16px;
       img {
-        width: 300px;
+        width: 250px;
       }
     }
     .content-info {
@@ -111,13 +117,8 @@ export function baseFeedback(
       width: 60%;
       padding: 16px 24px;
       margin-left: 24px;
-      .close-btn {
-        text-align: right;
-        cursor: pointer;
-        stroke: $primary;
-      }
       h2 {
-        margin-bottom: 24px;
+        margin-bottom: 16px;
       }
       p {
       }
@@ -130,9 +131,8 @@ export function baseFeedback(
   }
   .extra {
     margin-top: 24px;
-    padding: 16px 8px;
+    padding: 24px 8px;
     border-top: 1px solid $border;
-    margin-top: auto;
 
     p {
       font-size: 12px;
@@ -154,6 +154,44 @@ export function baseFeedback(
       }
       .copied {
         margin-left: 16px;
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .modal-content {
+    width: 90%;
+    margin-top: 5vh;
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      .wrap-img {
+        width: 100%;
+        padding: 0px;
+        flex-direction: column;
+        align-items: center;
+
+        img {
+          width: 50%;
+        }
+      }
+      .content-info {
+        width: 100%;
+        padding: 16px 24px;
+        margin-left: 0px;
+        text-align: center;
+
+        .extra {
+          .wrap-addr-btn {
+            flex-direction: column;
+            .copied {
+              margin-top: 8px;
+              margin-left: 0;
+            }
+          }
+        }
       }
     }
   }
