331f35b1a462be89b705d9538c4d4123e6212b4b
Davi Rodrigues <davirds07@gmail.com>
2022-02-13 20:36:20 -0300

    starting new lbp

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const FACTORY_CONTRACT = "terra1wzt06h3zrfy7rjhyxptfnc5f0r8r2jre2kp9fy";
-export const PAIR_CONTRACT = "terra1w8c8g6me57c0skejkhwyp75uqdurv48nze4waf";
+export const PAIR_CONTRACT = "terra1wdypxwgn9k79reamvs8hk3hjtlrrsv598lzsxf";
