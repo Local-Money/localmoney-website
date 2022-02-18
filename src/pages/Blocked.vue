ef6d3edd9821270507322d012d9cc074f9ae12cd
Gabriel Brettas <gabrielbrettas@gmail.com>
2022-02-18 03:40:45 -0300

    Implemented layout for the blocked page

--- src/pages/Blocked.vue
+++ src/pages/Blocked.vue
@@ -3,7 +3,29 @@
     <title>Local Terra</title>
   </head>
   <body>
-    <section></section>
+    <section class="section-spacer">
+      <div class="wrap page-layout">
+        <div class="wrap-img">
+          <img
+            src="@/assets/logo-horizontal-dark.svg"
+            alt="Local Terra Logo"
+            class="logo"
+          />
+        </div>
+        <div class="wrap-content">
+          <div class="wrap-img">
+            <img src="@/assets/img-02.png" alt="Local Terra Logo" />
+          </div>
+          <div class="content">
+            <h2>Ooops...</h2>
+            <p>
+              Unfortunately, the location you are trying to access the LBP is
+              not permited.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </body>
 </template>
 
@@ -39,4 +61,53 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 @import "@/style/app.scss";
+
+section {
+  display: flex;
+
+  .logo {
+    margin-bottom: 24px;
+  }
+
+  .wrap-content {
+    .wrap-img {
+      width: 30%;
+      img {
+        width: 100%;
+      }
+    }
+    .content {
+      width: 50%;
+      h2 {
+        margin-bottom: 16px;
+      }
+    }
+  }
+}
+@media only screen and (max-width: 600px) {
+  section {
+    display: flex;
+    text-align: center;
+
+    .logo {
+      margin-bottom: 32px;
+    }
+
+    .wrap-content {
+      .wrap-img {
+        width: 60%;
+        margin: 0 auto 16px;
+        img {
+          width: 100%;
+        }
+      }
+      .content {
+        width: 100%;
+        h2 {
+          margin-bottom: 16px;
+        }
+      }
+    }
+  }
+}
 </style>
