f3a8ca801be6ca9c9d9629eed7eda5750b77e5d1
Gabriel Brettas <gabrielbrettas@gmail.com>
2022-05-19 17:35:39 -0300

    Updated roadmap content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -330,57 +330,14 @@
                 />
               </svg>
             </div>
-            <p class="date">Q2 . 2022</p>
-          </div>
-          <div class="wrap-content">
-            <div class="content todo">
-              <p class="title">v1.0</p>
-              <p class="description">Security Audit</p>
-              <p class="description">
-                Within the first release of Local Terra, makers will be able to
-                create offers to buy and sell UST.
-              </p>
-            </div>
-          </div>
-        </div>
-        <!-- END ROADMAP ITEM -->
-
-        <div class="roadmap-item">
-          <div class="wrap-date">
-            <div class="icon-todo">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M21 16V8.00002C20.9996 7.6493 20.9071 7.30483 20.7315 7.00119C20.556 6.69754 20.3037 6.44539 20 6.27002L13 2.27002C12.696 2.09449 12.3511 2.00208 12 2.00208C11.6489 2.00208 11.304 2.09449 11 2.27002L4 6.27002C3.69626 6.44539 3.44398 6.69754 3.26846 7.00119C3.09294 7.30483 3.00036 7.6493 3 8.00002V16C3.00036 16.3508 3.09294 16.6952 3.26846 16.9989C3.44398 17.3025 3.69626 17.5547 4 17.73L11 21.73C11.304 21.9056 11.6489 21.998 12 21.998C12.3511 21.998 12.696 21.9056 13 21.73L20 17.73C20.3037 17.5547 20.556 17.3025 20.7315 16.9989C20.9071 16.6952 20.9996 16.3508 21 16Z"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M3.27 6.96002L12 12.01L20.73 6.96002"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M12 22.08V12"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
             <p class="date">Q3 . 2022</p>
           </div>
           <div class="wrap-content">
             <div class="content todo">
-              <p class="title">Luna and CW20 tokens support.</p>
+              <p class="title">v1.0</p>
               <p class="description">
-                We'll provide easy and quick fiat conversion to and from your
-                favorite Terra Alt by leveraging the existing UST pairs on
-                different AMMs and the composability of DeFi.
+                First release of LOCAL P2P where makers will be able to create
+                offers to buy and sell stablecoins.
               </p>
             </div>
           </div>
@@ -418,14 +375,10 @@
           </div>
           <div class="wrap-content">
             <div class="content todo">
-              <p class="title">IBC integration</p>
+              <p class="title">Governance</p>
               <p class="description">
-                (Inter-Blockchain Communication Protocol)
-              </p>
-              <p class="description">
-                Your Cosmos native asset will have a direct bridge with fiat
-                providers, powered by the combination of the best makers and
-                liquidity pools with the decentralized stablecoin, $UST.
+                Provide the community the ability to propose changes to the
+                protocol.
               </p>
             </div>
           </div>
@@ -465,8 +418,7 @@
             <div class="content todo">
               <p class="title">To the future and beyond</p>
               <p class="description">Mobile App</p>
-              <p class="description">Thorchain integration</p>
-              <p class="description">Deeper Anchor integration</p>
+              <p class="description">LOCAL Chain</p>
             </div>
           </div>
         </div>
@@ -483,7 +435,7 @@
           />
         </router-link>
         <div class="social">
-          <a href="https://twitter.com/local_terra" target="_blank">
+          <a href="https://twitter.com/local_p2p" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
