f752c1e0c5186676e95ff462884d143e026ab9c6
gabs <gabs@localmoney.io>
2022-10-14 16:53:39 -0300

    chore: updated content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -85,45 +85,6 @@
         </div>
       </section>
 
-      <!-- <section class="featured section-spacer">
-        <div class="wrap-featured page-layout">
-          <div class="feature">
-            <h2>
-              <span class="text-primary">Fast and easy</span> on-off ramps
-            </h2>
-            <div class="wrap-img">
-              <img
-                v-if="!isMobile"
-                src="@/assets/top-feature-01.png"
-                alt="Image displaying a top feature"
-              />
-              <img
-                v-else
-                src="@/assets/top-feature-01-mobile.png"
-                alt="Image displaying a top feature"
-              />
-            </div>
-          </div>
-          <div class="feature">
-            <h2>
-              <span class="text-primary">Trustless</span> trading experience
-            </h2>
-            <div class="wrap-img">
-              <img
-                v-if="!isMobile"
-                src="@/assets/top-feature-02.png"
-                alt="Image displaying a top feature"
-              />
-              <img
-                v-else
-                src="@/assets/top-feature-02-mobile.png"
-                alt="Image displaying a top feature"
-              />
-            </div>
-          </div>
-        </div>
-      </section> -->
-
       <section class="team section-spacer">
         <div class="wrap-team page-layout">
           <h2>Meet the <span class="text-primary">team</span></h2>
@@ -358,24 +319,10 @@
 
         <div class="roadmap-item">
           <div class="wrap-date">
-            <div class="icon-todo">
+            <div class="icon-done">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path
-                  d="M21 16V8.00002C20.9996 7.6493 20.9071 7.30483 20.7315 7.00119C20.556 6.69754 20.3037 6.44539 20 6.27002L13 2.27002C12.696 2.09449 12.3511 2.00208 12 2.00208C11.6489 2.00208 11.304 2.09449 11 2.27002L4 6.27002C3.69626 6.44539 3.44398 6.69754 3.26846 7.00119C3.09294 7.30483 3.00036 7.6493 3 8.00002V16C3.00036 16.3508 3.09294 16.6952 3.26846 16.9989C3.44398 17.3025 3.69626 17.5547 4 17.73L11 21.73C11.304 21.9056 11.6489 21.998 12 21.998C12.3511 21.998 12.696 21.9056 13 21.73L20 17.73C20.3037 17.5547 20.556 17.3025 20.7315 16.9989C20.9071 16.6952 20.9996 16.3508 21 16Z"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M3.27 6.96002L12 12.01L20.73 6.96002"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M12 22.08V12"
+                  d="M20 6L9 17L4 12"
                   stroke="inherit"
                   stroke-width="2"
                   stroke-linecap="round"
@@ -383,11 +330,11 @@
                 />
               </svg>
             </div>
-            <p class="date">Q3 . 2022</p>
+            <p class="date">Oct 14th, 2022</p>
           </div>
           <div class="wrap-content">
             <div class="content todo">
-              <p class="title">Planned Testnet Update</p>
+              <p class="title">Testnet Update</p>
               <p class="description">
                 • Arbitration System, that will deal with trade disputes.
               </p>
@@ -431,7 +378,7 @@
                 />
               </svg>
             </div>
-            <p class="date">Q3 . 2022</p>
+            <p class="date">Q4 . 2022</p>
           </div>
           <div class="wrap-content">
             <div class="content todo">
@@ -472,52 +419,12 @@
                 />
               </svg>
             </div>
-            <p class="date">Q4 . 2022</p>
-          </div>
-          <div class="wrap-content">
-            <div class="content todo">
-              <p class="title">Governance</p>
-              <p class="description">
-                Provide the community with the ability to propose changes to the
-                protocol.
-              </p>
-            </div>
-          </div>
-        </div>
-        <!-- END ROADMAP ITEM -->
-
-        <div class="roadmap-item">
-          <div class="wrap-date">
-            <div class="icon-todo">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M21 16V8.00002C20.9996 7.6493 20.9071 7.30483 20.7315 7.00119C20.556 6.69754 20.3037 6.44539 20 6.27002L13 2.27002C12.696 2.09449 12.3511 2.00208 12 2.00208C11.6489 2.00208 11.304 2.09449 11 2.27002L4 6.27002C3.69626 6.44539 3.44398 6.69754 3.26846 7.00119C3.09294 7.30483 3.00036 7.6493 3 8.00002V16C3.00036 16.3508 3.09294 16.6952 3.26846 16.9989C3.44398 17.3025 3.69626 17.5547 4 17.73L11 21.73C11.304 21.9056 11.6489 21.998 12 21.998C12.3511 21.998 12.696 21.9056 13 21.73L20 17.73C20.3037 17.5547 20.556 17.3025 20.7315 16.9989C20.9071 16.6952 20.9996 16.3508 21 16Z"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M3.27 6.96002L12 12.01L20.73 6.96002"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M12 22.08V12"
-                  stroke="inherit"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
             <p class="date">2023</p>
           </div>
           <div class="wrap-content">
             <div class="content todo">
               <p class="title">To the future and beyond</p>
+              <p class="description">Governance</p>
               <p class="description">Mobile App</p>
               <p class="description">LOCAL Chain</p>
             </div>
@@ -552,6 +459,21 @@
                 d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
               ></path></svg
           ></a>
+
+          <a href="https://t.co/P16BhQpcyc" target="_blank">
+            <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M18.942 5.55597C17.6304 4.94227 16.2429 4.5061 14.816 4.25897C14.638 4.57997 14.431 5.01297 14.287 5.35597C12.7714 5.12497 11.2296 5.12497 9.71401 5.35597C9.55508 4.98116 9.3765 4.61498 9.17901 4.25897C7.75093 4.50651 6.36231 4.94371 5.05001 5.55897C2.43901 9.50497 1.73101 13.353 2.08501 17.146C3.60917 18.2968 5.32161 19.1742 7.14601 19.739C7.55592 19.1749 7.91849 18.5778 8.23001 17.954C7.63749 17.7282 7.06623 17.4501 6.52301 17.123C6.66601 17.017 6.80601 16.906 6.94101 16.792C10.232 18.331 13.807 18.331 17.059 16.792C17.196 16.906 17.336 17.017 17.477 17.123C16.936 17.449 16.363 17.729 15.767 17.955C16.078 18.5791 16.4406 19.1762 16.851 19.74C18.6769 19.1755 20.3905 18.2974 21.915 17.145C22.33 12.749 21.206 8.93597 18.942 5.55597V5.55597ZM8.67801 14.813C7.69001 14.813 6.88001 13.891 6.88001 12.768C6.88001 11.645 7.67301 10.721 8.67801 10.721C9.68301 10.721 10.493 11.643 10.476 12.768C10.477 13.891 9.68301 14.813 8.67801 14.813ZM15.322 14.813C14.334 14.813 13.524 13.891 13.524 12.768C13.524 11.645 14.317 10.721 15.322 10.721C16.327 10.721 17.137 11.643 17.12 12.768C17.12 13.891 16.327 14.813 15.322 14.813Z"
+                  fill="inherit"
+                />
+              </svg>
+          </a>
 
           <a href="https://github.com/Local-Money" target="_blank">
             <svg
